50ba55ac365eae9f5edae58809a127d6294ce39a
Ivan Onopa <ivanonopa@gmail.com>
2018-08-22 18:17:12 +0300

    ДЗ №6

---
+++ L61cache/src/main/java/ru/otus/l61/CacheEngine.java
@@ -0,0 +1,15 @@
+package ru.otus.l61;
+
+
+public interface CacheEngine<K, V> {
+
+    void put(MyElement<K, V> element);
+
+    MyElement<K, V> get(K key);
+
+    int getHitCount();
+
+    int getMissCount();
+
+    void dispose();
+}
---
+++ L61cache/src/main/java/ru/otus/l61/CacheMain.java
@@ -0,0 +1,59 @@
+package ru.otus.l61;
+
+
+public class CacheMain {
+
+    public static void main(String[] args) throws InterruptedException {
+        //new CacheMain().eternalCacheExample();
+        new CacheMain().lifeCacheExample();
+    }
+
+    private void eternalCacheExample() {
+        int size = 15;
+        CacheEngine<Integer, String> cache = new CacheEngineImpl<>(size, 0, 0, true);
+
+        for (int i = 0; i < 10; i++) {
+            cache.put(new MyElement<>(i, "String: " + i));
+        }
+
+        for (int i = 0; i < 10; i++) {
+            MyElement<Integer, String> element = cache.get(i);
+            System.out.println("String for " + i + ": " + (element != null ? element.getValue() : "null"));
+        }
+
+        System.out.println("Cache hits: " + cache.getHitCount());
+        System.out.println("Cache misses: " + cache.getMissCount());
+
+        cache.dispose();
+    }
+
+    private void lifeCacheExample() throws InterruptedException {
+        int size = 10000000;
+        CacheEngine<Integer, String> cache = new CacheEngineImpl<>(size, 1000, 0, false);
+
+        for (int i = 0; i < size; i++) {
+            cache.put(new MyElement<>(i, "String: " + i));
+        }
+
+        for (int i = 0; i < size; i++) {
+            MyElement<Integer, String> element = cache.get(i);
+            //System.out.println("String for " + i + ": " + (element != null ? element.getValue() : "null"));
+        }
+
+        System.out.println("Cache hits: " + cache.getHitCount());
+        System.out.println("Cache misses: " + cache.getMissCount());
+
+        Thread.sleep(1000);
+
+        for (int i = 0; i < size; i++) {
+            MyElement<Integer, String> element = cache.get(i);
+           // System.out.println("String for " + i + ": " + (element != null ? element.getValue() : "null"));
+        }
+
+        System.out.println("Cache hits: " + cache.getHitCount());
+        System.out.println("Cache misses: " + cache.getMissCount());
+
+        cache.dispose();
+    }
+
+}
