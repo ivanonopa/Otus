5ea2fc23c381aebf448afb99ee130b921c0aa1fd
Ivan Onopa <ivanonopa@gmail.com>
2018-08-27 11:37:32 +0300

    Подключил логгирование.

--- L21/src/main/java/ru/otus/l21/Main.java
+++ L21/src/main/java/ru/otus/l21/Main.java
@@ -1,5 +1,8 @@
 package ru.otus.l21;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.lang.management.ManagementFactory;
 import java.util.ArrayList;
 import java.util.function.Supplier;
@@ -18,33 +21,31 @@ import java.util.function.Supplier;
  */
 @SuppressWarnings({"RedundantStringConstructorCall", "InfiniteLoopStatement"})
 public class Main {
+
+    private static Logger logger = LoggerFactory.getLogger(Main.class);
+
     public static void main(String... args) throws InterruptedException {
 
-        calcSize(() -> new String());
-
-        calcSize(() -> new ArrayList());
-        System.out.println("ArrayList(10):");
-        calcSize(() -> new ArrayList(10));
-        System.out.println("ArrayList(20):");
-        calcSize(() -> new ArrayList(20));
-        System.out.println("ArrayList(30):");
-        calcSize(() -> new ArrayList(30));
-        System.out.println("ArrayList(50):");
-        calcSize(() -> new ArrayList(100));
+        System.out.println("String: " + calcSize(() -> new String()));
+        System.out.println("Arraylist: " +calcSize(() -> new ArrayList()));
+        System.out.println("ArrayList(10): " + calcSize(() -> new ArrayList(10)));
+        System.out.println("ArrayList(20): " + calcSize(() -> new ArrayList(20)));
+        System.out.println("ArrayList(30): " + calcSize(() -> new ArrayList(30)));
+        System.out.println("ArrayList(100): " + calcSize(() -> new ArrayList(100)));
     }
 
     private static long calcSize(Supplier supplier) throws InterruptedException {
-        System.out.println("pid: " + ManagementFactory.getRuntimeMXBean().getName());
+        logger.info("pid: {}", ManagementFactory.getRuntimeMXBean().getName());
 
         int size = 1_000_000;
 
         long mem = getMem();
-        System.out.println("Mem: " + mem);
+        logger.info("Mem: {}", mem);
 
         Object[] array = new Object[size];
 
         long mem2 = getMem();
-        System.out.println("Ref size: " + (mem2 - mem) / array.length);
+        logger.info("Ref size: {}", (mem2 - mem) / array.length);
 
         for (int i = 0; i < array.length; i++) {
             array[i] = supplier.get();
@@ -52,10 +53,9 @@ public class Main {
 
         long mem3 = getMem();
         long objectSize = (mem3 - mem2) / array.length;
-        System.out.println("Element size for " + supplier.get().getClass().getName() + " is " + objectSize);
+        logger.info("Element size for {} is {}", supplier.get().getClass().getName(), objectSize);
 
         array = null;
-        System.out.println("Array is ready for GC");
 
         return objectSize;
     }
